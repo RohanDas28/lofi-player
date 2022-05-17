5bf7611e71795d5ea9b5d5f45be06d8af30416f9
scorcism <abhishekpathak1720@gmail.com>
2022-05-17 12:07:52 +0530

    Add logic to control volume using range

--- lofi.js
+++ lofi.js
@@ -2,10 +2,16 @@ const mymusic = new Audio();
 mymusic.src = "https://rohandas28.github.io/lofi-player/music/1.mp3";
 
 var button = document.getElementById("button_1");
+
+const rangechange = (val) =>{
+  var k = val/10
+  mymusic.volume = k 
+}
+
 button.addEventListener("click", function(){
   if(mymusic.paused){
     mymusic.play();
-    mymusic.volume=.4;
+    
     button.className = "fa-solid fa-pause fa-beat";
   } else {
     mymusic.pause();
@@ -24,3 +30,11 @@ setInterval(() =>{
 },10000
 )
 
+// let sliderValue = document.getElementById("myRange");
+
+// sliderValue.oninput = function() {
+//   let k =("0." + (sliderValue.value%10))
+//   k  =parseInt(k)
+//   console.log(k)
+//   console.log("0." + sliderValue.value%10)
+// }
